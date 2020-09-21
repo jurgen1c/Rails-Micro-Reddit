ef78871de77651c3c1c905370dd5b05f5ab7a8d8
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-22 00:48:25 +0300

    Add Post model and associate it with User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, :email, :crypted_password, :presence => true
   validates :username, :email, :length => { :minimum => 3, :maximum => 30 }
+
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_203300) do
+ActiveRecord::Schema.define(version: 2020_09_21_212359) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
