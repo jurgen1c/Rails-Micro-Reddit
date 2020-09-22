2efc7505901ddcb7f3f828d5611e2eeaa2dfeb1a
jurgen1c <jurgen1c@gmail.com>
2020-09-22 10:19:08 -0600

    Fix linters errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :body, :post, :user, presence: true
-  validates :body, :length => {:minimum => 5, :maximum => 150}
+  validates :body, length: { minimum: 5, maximum: 150 }
   belongs_to :post
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, :presence => true
-  validates :password, :length => { :minimum => 3, :maximum => 30 }, :presence => true
+  validates :name, presence: true
+  validates :password, length: { minimum: 3, maximum: 30 }, presence: true
 
   has_many :posts
   has_many :comments
