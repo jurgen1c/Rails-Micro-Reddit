2ab718cf322a29ade616952a987ace7764d287b9
jurgen1c <jurgen1c@gmail.com>
2020-09-22 10:10:57 -0600

    Correct table fields & migrate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  validates :body, presence: true
+  validates :body, :post, :user, presence: true
+  validates :body, :length => {:minimum => 5, :maximum => 150}
   belongs_to :post
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_142331) do
+ActiveRecord::Schema.define(version: 2020_09_22_155938) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
